23581f504e80de9d2cadee30f00c64dcdb8b0939
dev-arjunan <50161510+dev-arjunan@users.noreply.github.com>
2020-06-04 09:34:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
-(function(d){
-    function C(k){return(d.cookie.match('(^|; )'+k+'=([^;]*)')||0)[2];}
- 
-    var ua = navigator.userAgent, 
-        ismobile = / mobile/i.test(ua),
-        mgecko = !!( / gecko/i.test(ua) && / firefox\//i.test(ua)),
-        wasmobile = C('wasmobile') === "was", 
-        desktopvp = 'user-scalable=yes, maximum-scale=2',
-        el;
- 
-    if(ismobile && !wasmobile){
-        d.cookie = "wasmobile=was"; 
-    }
-    else if (!ismobile && wasmobile){
-        
-        if (mgecko) {
-            el = d.createElement('meta');
-            el.setAttribute('content',desktopvp);
-            el.setAttribute('name','viewport');
-            d.getElementsByTagName('head')[0].appendChild( el );
-        }else{
-            d.getElementsByName('viewport')[0].setAttribute('content',desktopvp); 
-        }
-    }
-}(document));
+var browser = navigator.userAgent.toLowerCase();
+if (browser.indexOf('firefox') > -1) {
+   console.log('firefox');
+    var el;
+    var desktopvp = 'user-scalable=yes, maximum-scale=2';
+    el=document.reateElement('meta');
+    el.setAttribute('content',desktopvp);
+    el.setAttribute('name','viewport');
+}
+
+
