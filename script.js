909c6a61336eb115fca316ef65416dfbec0de937
dev-arjunan <50161510+dev-arjunan@users.noreply.github.com>
2020-06-04 10:16:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var browser = navigator.userAgent.toLowerCase();
 if (browser.indexOf('firefox') > -1) {
    console.log('firefox');
     var el;
-    var desktopvp = 'user-scalable=yes, maximum-scale=2';
+    var desktopvp = 'width=1200';
     el=document.createElement('meta');
     el.setAttribute('content',desktopvp);
     el.setAttribute('name','viewport');
