3de1598b9e038d9706e57c91baf88749883cef51
dev-arjunan <50161510+dev-arjunan@users.noreply.github.com>
2020-08-02 14:52:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-var browser = navigator.userAgent.toLowerCase();
-if (browser.indexOf('firefox') > -1) {
-   console.log('firefox');
-    var el;
-    var desktopvp = 'width=1200';
-    el=document.createElement('meta');
-    el.setAttribute('content',desktopvp);
-    el.setAttribute('name','viewport');
-   document.getElementsByTagName("head")[0].appendChild(el);
+function showresult(choise){
+var n1=parseFloat(document.getElementById('num1').value);
+var n2=parseFloat(document.getElementById('num2').value);
+var r;
+var c=choise;
+
+switch(c)
+	{
+	case '1':
+		r=n1+n2;
+		break;
+	case '2':
+		r=n1-n2;
+		break;
+	case '3':
+		r=n1*n2;
+		break;
+	case '4': 
+		r=n1/n2;
+		break;
+	case '5':
+		r=n2*100/n1;
+		break;
+	default:
+		break;
+			
+	}
+document.getElementById('result').value=r;
+
+	
+
 }
-
-
